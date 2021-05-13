12c7450f3bb27bfca8259849bb60f028c85e86f1
dhaas01 <77082867+dhaas01@users.noreply.github.com>
2021-05-12 22:03:58 -0400

    Add files via upload

---
+++ Project 1/Project 1/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.exitButton = new System.Windows.Forms.Button();
+            this.CPUPaperPictureBox = new System.Windows.Forms.PictureBox();
+            this.YourChoice = new System.Windows.Forms.Label();
+            this.yourPaperPictureBox = new System.Windows.Forms.PictureBox();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.CPUsChoice = new System.Windows.Forms.Label();
+            this.yourScoreLabel = new System.Windows.Forms.Label();
+            this.CPUScoreLabel = new System.Windows.Forms.Label();
+            this.drawCountLabel = new System.Windows.Forms.Label();
+            this.yourScissorsPictureBox = new System.Windows.Forms.PictureBox();
+            this.yourRockPictureBox = new System.Windows.Forms.PictureBox();
+            this.CPUScissorsPictureBox = new System.Windows.Forms.PictureBox();
+            this.CPURockPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUPaperPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourPaperPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourScissorsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourRockPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUScissorsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CPURockPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(214, 264);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // CPUPaperPictureBox
+            // 
+            this.CPUPaperPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("CPUPaperPictureBox.Image")));
+            this.CPUPaperPictureBox.Location = new System.Drawing.Point(284, 81);
+            this.CPUPaperPictureBox.Name = "CPUPaperPictureBox";
+            this.CPUPaperPictureBox.Size = new System.Drawing.Size(106, 105);
+            this.CPUPaperPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CPUPaperPictureBox.TabIndex = 1;
+            this.CPUPaperPictureBox.TabStop = false;
+            this.CPUPaperPictureBox.Visible = false;
+            // 
+            // YourChoice
+            // 
+            this.YourChoice.AutoSize = true;
+            this.YourChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YourChoice.Location = new System.Drawing.Point(150, 38);
+            this.YourChoice.Name = "YourChoice";
+            this.YourChoice.Size = new System.Drawing.Size(35, 16);
+            this.YourChoice.TabIndex = 2;
+            this.YourChoice.Text = "You";
+            // 
+            // yourPaperPictureBox
+            // 
+            this.yourPaperPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("yourPaperPictureBox.Image")));
+            this.yourPaperPictureBox.Location = new System.Drawing.Point(123, 81);
+            this.yourPaperPictureBox.Name = "yourPaperPictureBox";
+            this.yourPaperPictureBox.Size = new System.Drawing.Size(109, 105);
+            this.yourPaperPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yourPaperPictureBox.TabIndex = 3;
+            this.yourPaperPictureBox.TabStop = false;
+            this.yourPaperPictureBox.Visible = false;
+            // 
+            // rockButton
+            // 
+            this.rockButton.Location = new System.Drawing.Point(34, 105);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(58, 23);
+            this.rockButton.TabIndex = 4;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.RockButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.Location = new System.Drawing.Point(34, 134);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(58, 23);
+            this.paperButton.TabIndex = 5;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.PaperButton_Click);
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.Location = new System.Drawing.Point(34, 163);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(58, 23);
+            this.scissorsButton.TabIndex = 6;
+            this.scissorsButton.Text = "Scissors ";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.ScissorsButton_Click);
+            // 
+            // CPUsChoice
+            // 
+            this.CPUsChoice.AutoSize = true;
+            this.CPUsChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPUsChoice.Location = new System.Drawing.Point(318, 38);
+            this.CPUsChoice.Name = "CPUsChoice";
+            this.CPUsChoice.Size = new System.Drawing.Size(39, 16);
+            this.CPUsChoice.TabIndex = 7;
+            this.CPUsChoice.Text = "CPU";
+            // 
+            // yourScoreLabel
+            // 
+            this.yourScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.yourScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yourScoreLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.yourScoreLabel.Location = new System.Drawing.Point(12, 20);
+            this.yourScoreLabel.Name = "yourScoreLabel";
+            this.yourScoreLabel.Size = new System.Drawing.Size(132, 41);
+            this.yourScoreLabel.TabIndex = 11;
+            this.yourScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CPUScoreLabel
+            // 
+            this.CPUScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CPUScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPUScoreLabel.ForeColor = System.Drawing.Color.Red;
+            this.CPUScoreLabel.Location = new System.Drawing.Point(363, 20);
+            this.CPUScoreLabel.Name = "CPUScoreLabel";
+            this.CPUScoreLabel.Size = new System.Drawing.Size(125, 41);
+            this.CPUScoreLabel.TabIndex = 12;
+            this.CPUScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // drawCountLabel
+            // 
+            this.drawCountLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.drawCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drawCountLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.drawCountLabel.Location = new System.Drawing.Point(321, 205);
+            this.drawCountLabel.Name = "drawCountLabel";
+            this.drawCountLabel.Size = new System.Drawing.Size(178, 67);
+            this.drawCountLabel.TabIndex = 13;
+            this.drawCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yourScissorsPictureBox
+            // 
+            this.yourScissorsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("yourScissorsPictureBox.Image")));
+            this.yourScissorsPictureBox.Location = new System.Drawing.Point(123, 81);
+            this.yourScissorsPictureBox.Name = "yourScissorsPictureBox";
+            this.yourScissorsPictureBox.Size = new System.Drawing.Size(109, 105);
+            this.yourScissorsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yourScissorsPictureBox.TabIndex = 14;
+            this.yourScissorsPictureBox.TabStop = false;
+            this.yourScissorsPictureBox.Visible = false;
+            // 
+            // yourRockPictureBox
+            // 
+            this.yourRockPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("yourRockPictureBox.Image")));
+            this.yourRockPictureBox.Location = new System.Drawing.Point(123, 81);
+            this.yourRockPictureBox.Name = "yourRockPictureBox";
+            this.yourRockPictureBox.Size = new System.Drawing.Size(109, 105);
+            this.yourRockPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yourRockPictureBox.TabIndex = 15;
+            this.yourRockPictureBox.TabStop = false;
+            this.yourRockPictureBox.Visible = false;
+            // 
+            // CPUScissorsPictureBox
+            // 
+            this.CPUScissorsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("CPUScissorsPictureBox.Image")));
+            this.CPUScissorsPictureBox.Location = new System.Drawing.Point(284, 81);
+            this.CPUScissorsPictureBox.Name = "CPUScissorsPictureBox";
+            this.CPUScissorsPictureBox.Size = new System.Drawing.Size(106, 105);
+            this.CPUScissorsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CPUScissorsPictureBox.TabIndex = 16;
+            this.CPUScissorsPictureBox.TabStop = false;
+            this.CPUScissorsPictureBox.Visible = false;
+            // 
+            // CPURockPictureBox
+            // 
+            this.CPURockPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("CPURockPictureBox.Image")));
+            this.CPURockPictureBox.Location = new System.Drawing.Point(284, 81);
+            this.CPURockPictureBox.Name = "CPURockPictureBox";
+            this.CPURockPictureBox.Size = new System.Drawing.Size(109, 105);
+            this.CPURockPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CPURockPictureBox.TabIndex = 17;
+            this.CPURockPictureBox.TabStop = false;
+            this.CPURockPictureBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 299);
+            this.Controls.Add(this.CPURockPictureBox);
+            this.Controls.Add(this.CPUScissorsPictureBox);
+            this.Controls.Add(this.yourRockPictureBox);
+            this.Controls.Add(this.yourScissorsPictureBox);
+            this.Controls.Add(this.drawCountLabel);
+            this.Controls.Add(this.CPUScoreLabel);
+            this.Controls.Add(this.yourScoreLabel);
+            this.Controls.Add(this.CPUsChoice);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.yourPaperPictureBox);
+            this.Controls.Add(this.YourChoice);
+            this.Controls.Add(this.CPUPaperPictureBox);
+            this.Controls.Add(this.exitButton);
+            this.Name = "Form1";
+            this.Text = "Rock, Paper, Scissors Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CPUPaperPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourPaperPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourScissorsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yourRockPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUScissorsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CPURockPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox CPUPaperPictureBox;
+        private System.Windows.Forms.Label YourChoice;
+        private System.Windows.Forms.PictureBox yourPaperPictureBox;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Label CPUsChoice;
+        private System.Windows.Forms.Label yourScoreLabel;
+        private System.Windows.Forms.Label CPUScoreLabel;
+        private System.Windows.Forms.Label drawCountLabel;
+        private System.Windows.Forms.PictureBox yourScissorsPictureBox;
+        private System.Windows.Forms.PictureBox yourRockPictureBox;
+        private System.Windows.Forms.PictureBox CPUScissorsPictureBox;
+        private System.Windows.Forms.PictureBox CPURockPictureBox;
+    }
+}
+
---
+++ Project 1/Project 1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1
+{
+    public partial class Form1 : Form
+    {
+        int playerWins = 0; // Will hold the number of times the player wins.
+        int playerChoice;   // Will hold the value of the player's choice whether it be rock, paper, scissors.
+        int cpuWins = 0;    // Will hold the number of times the computer wins.
+        int draws = 0;      // Will hold the number of times the computer and player draw.
+        int cpuChoice;      // Will hold the random number that is generated for the computer.
+        public Form1()
+        {
+             ();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerChoice = 0;
+        }
+        // Function determines who wins and keeps track of number of wins and draws and selections.
+        private void Winner()
+        { 
+            // The if statements determine who will win in each round. 1 is rock, 2 is paper, 3 is scissors.
+            if ((cpuChoice == 1) && (playerChoice == 2))
+            {
+                CPURockPictureBox.Visible = true;       // The picture will be shown for the rock.
+                CPUPaperPictureBox.Visible = false;     // The pictures for scissors and paper will be hidden.
+                CPUScissorsPictureBox.Visible = false;
+                playerWins++;                           // In this round, the player won so it will keep track of that.
+                MessageBox.Show("You win! Paper beats rock.");
+            } 
+            if ((cpuChoice == 1) && (playerChoice == 3))
+            {
+                CPURockPictureBox.Visible = true;       // Only the rock picture will be shown, others hidden.
+                CPUPaperPictureBox.Visible = false;
+                CPUScissorsPictureBox.Visible = false;
+                cpuWins++;                              // In this round, computer won so it will keep track of that.
+                MessageBox.Show("CPU wins! Rock beats scissors.");
+            } 
+            if ((cpuChoice == 2) && (playerChoice == 1))
+            {
+                CPUPaperPictureBox.Visible = true;      // Only the paper image is shown.
+                CPURockPictureBox.Visible = false;
+                CPUScissorsPictureBox.Visible = false;
+                cpuWins++;
+                MessageBox.Show("CPU wins! Paper beats rock.");
+            } 
+            if ((cpuChoice == 2) && (playerChoice == 3))
+            {
+                CPUPaperPictureBox.Visible = true;
+                CPURockPictureBox.Visible = false;
+                CPUScissorsPictureBox.Visible = false;
+                playerWins++;
+                MessageBox.Show("You win! Scissors beats paper.");
+            }
+            if ((cpuChoice == 3) && (playerChoice == 1))
+            {
+                CPUScissorsPictureBox.Visible = true;   // Only the scissors image is shown.
+                CPURockPictureBox.Visible = false;
+                CPUPaperPictureBox.Visible = false;
+                playerWins++;
+                MessageBox.Show("You win! Rock beats scissors.");
+            }
+            if ((cpuChoice == 3) && (playerChoice == 2))
+            {
+                CPUScissorsPictureBox.Visible = true;
+                CPURockPictureBox.Visible = false;
+                CPUPaperPictureBox.Visible = false;
+                cpuWins++;
+                MessageBox.Show("CPU wins! Scissors beats paper.");
+            } 
+            if (cpuChoice == playerChoice) // If the computer and play pick the same item, this is executed.
+            {
+                if (cpuChoice == 1)        // Since it is nested inside another if statement, both have rock selected.
+                {
+                    CPURockPictureBox.Visible = true;
+                    CPUPaperPictureBox.Visible = false;
+                    CPUScissorsPictureBox.Visible = false;
+                } 
+                else if (cpuChoice == 2)    // Both have paper selected.
+                {
+                    CPUPaperPictureBox.Visible = true;
+                    CPURockPictureBox.Visible = false;
+                    CPUScissorsPictureBox.Visible = false;
+                } 
+                else if (cpuChoice == 3)    // Both have scissors selected.
+                {
+                    CPUScissorsPictureBox.Visible = true;
+                    CPURockPictureBox.Visible = false;
+                    CPUPaperPictureBox.Visible = false;
+                }
+                draws++;                    // Each time that both pick the same item, draws++ keeps track of the count.
+                MessageBox.Show("Your choice is the same as the CPU. Play again.");
+            }
+            yourScoreLabel.Text = "Score: " + playerWins;   // Will display the number of times player wins.
+            CPUScoreLabel.Text = "Score: " + cpuWins;       // Will display the number of times computer wins.
+            drawCountLabel.Text = "Number of draws: " + draws;  // Will display the number of times they draw.
+        } 
+        // Generates a choice for the computer.
+        private void Generate_CPU_Choice()
+        {   // Randomizes the choice between 1 and 3.
+            Random rand = new Random();
+            cpuChoice = rand.Next(1, 4);
+        }
+        // The user selects rock, this will be executed.
+        private void RockButton_Click(object sender, EventArgs e)
+        {
+            playerChoice = 1;                       // Player chose rock, rock is 1.
+            yourRockPictureBox.Visible = true;      // Will only show the rock image.
+            yourPaperPictureBox.Visible = false;
+            yourScissorsPictureBox.Visible = false;
+            Generate_CPU_Choice();                  // Calls the computer's choice 
+            Winner();                               // Calls the function that determines who wins, etc.
+        }
+        // The user selects paper, this will be executed.
+        private void PaperButton_Click(object sender, EventArgs e)
+        {
+            playerChoice = 2;                       // Player chose paper, paper is 2.
+            yourPaperPictureBox.Visible = true;     // Will only show the paper image.
+            yourScissorsPictureBox.Visible = false;
+            yourRockPictureBox.Visible = false;
+            Generate_CPU_Choice();                  // Calls the computer's choice.
+            Winner();                               // Calls the function that determines who wins, etc.
+        }
+        // The user selects scissors, this will be executed.
+        private void ScissorsButton_Click(object sender, EventArgs e)
+        {
+            playerChoice = 3;                       // The player chose scissors, scissors is 3.
+            yourScissorsPictureBox.Visible = true;  // Will only show the image of the scissors.
+            yourPaperPictureBox.Visible = false;
+            yourRockPictureBox.Visible = false;
+            Generate_CPU_Choice();                  // Calls the computer's choice.
+            Winner();                               // Calls the function that determines who wins, etc.
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
